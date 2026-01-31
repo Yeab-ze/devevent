a85c55c0946682b85768e3f36868c5b6a1818a1e
Tech-Nerd <yeabsiragetachew04@gmail.com>
2026-01-30 18:42:56 -0800

    finished the navbar and hero section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
 import "./globals.css";
+import LightRays from "@/components/LightRays";
+import Navbar from "@/components/Navbar";
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: "--font-schibsted-grotesk",
@@ -14,7 +16,7 @@ const martianMono = Martian_Mono({
 
 export const metadata: Metadata = {
   title: "DevEvent",
-  description: "make it enjoy with DevEvent",
+  description: "The Hub for Every Dev Event You Mustn't Miss",
 };
 
 export default function RootLayout({
@@ -27,7 +29,25 @@ export default function RootLayout({
       <body
         className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
       >
-        {children}
+        <Navbar/>
+
+          <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+              <LightRays
+                  raysOrigin="top-center-offset"
+                  raysColor="#5dfeca"
+                  raysSpeed={0.5}
+                  lightSpread={0.9}
+                  rayLength={1.4}
+                  followMouse={true}
+                  mouseInfluence={0.02}
+                  noiseAmount={0.0}
+                  distortion={0.01}
+              />
+          </div>
+
+          <main>
+            {children}
+          </main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
+import EventBtn from '@/components/EventBtn'
 import React from 'react'
 
 const page = () => {
   return (
-    <h1>WElcome to ne</h1>
+   <section>
+            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
+            <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+
+            <EventBtn />
+
+            <div className="mt-20 space-y-7">
+                <h3>Featured Events</h3>
+
+                <ul className="events">
+                    {events && events.length > 0 && events.map((event: IEvent) => (
+                        <li key={event.title} className="list-none">
+                            <EventCard {...event} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>
   )
 }
 
